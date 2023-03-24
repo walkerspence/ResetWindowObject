6390078819588ed4307e757d6f66196fbc34ce16
Walker Spence <walker@formswift.com>
2023-03-24 15:17:54 -0700

    test: successfully remove window and document event listeners

--- index.test.js
+++ index.test.js
@@ -1,26 +1,86 @@
-import { screen, render } from '@testing-library/react'
-import { useEffect } from 'react'
+import { screen, render, fireEvent } from '@testing-library/react'
+import { useEffect, useRef } from 'react'
+
+const wait = (duration = 0) =>
+  new Promise((resolve) => setTimeout(resolve, duration));
 
 const MyComponent = ({ set }) => {
+  const divRef = useRef(undefined)
+
   useEffect(() => {
     if (set) {
       window.something = 'hi';
+
+      window.addEventListener('click', () => {
+        console.log('clicked window')
+        if (!divRef.current) {
+          throw new Error('No value for .current on divRef in the window listener. This probably means the listener persisted from a previous test case and the div has since been unmounted.')
+        }
+        divRef.current.setAttribute('clicked-window', 'true')
+      });
+
+      document.addEventListener('click', () => {
+        console.log('clicked document')
+        if (!divRef.current) {
+          throw new Error('No value for .current on divRef in the document listener. This probably means the listener persisted from a previous test case and the div has since been unmounted.')
+        }
+        divRef.current.setAttribute('clicked-document', 'true')
+      });
     }
   }, [set])
 
-  return <div>Hello World</div>
+  return <div ref={divRef}>Hello World</div>
 }
 
-describe('my describe', () => {
-  test('my test', () => {
+describe('my describe',  () => {
+  test('my test 1',async () => {
+    console.log('--------------------------TEST 1--------------------------');
     render(<MyComponent set={true} />)
-    expect(window.something).toEqual('hi')
     screen.getByText('Hello World')
+    expect(window.something).toEqual('hi')
+
+    fireEvent(
+      window,
+      new MouseEvent('click', {
+        bubbles: false,
+        cancelable: true,
+      }),
+    )
+    fireEvent(
+      document,
+      new MouseEvent('click', {
+        bubbles: false,
+        cancelable: true,
+      }),
+    )
+    expect(screen.getByText('Hello World').getAttribute('clicked-window')).toEqual('true')
+    expect(screen.getByText('Hello World').getAttribute('clicked-document')).toEqual('true')
+    console.log('----------------------------END --------------------------');
+
   })
 
-  test('my test', () => {
+  test('my test 2', async () => {
+    console.log('--------------------------TEST 2--------------------------');
     render(<MyComponent set={false} />)
-    expect(window.something).toEqual(undefined)
     screen.getByText('Hello World')
+    expect(window.something).toEqual('hi') // BAD
+
+    fireEvent(
+      window,
+      new MouseEvent('click', {
+        bubbles: false,
+        cancelable: true,
+      }),
+    )
+    fireEvent(
+      document,
+      new MouseEvent('click', {
+        bubbles: false,
+        cancelable: true,
+      }),
+    )
+    expect(screen.getByText('Hello World').getAttribute('clicked-window')).toEqual(null)
+    expect(screen.getByText('Hello World').getAttribute('clicked-document')).toEqual(null)
+    console.log('----------------------------END --------------------------');
   })
 })
--- setup.js
+++ setup.js
@@ -1,106 +1,58 @@
 import { JSDOM } from 'jsdom';
-/* MY ATTEMPT */
+/* OLD ATTEMPT */
+// beforeEach(() => {
+//   // Settings match my jest.config.js JSDOM config
+//   const defaultJSDOMSettings = {
+//     contentType: 'text/html',
+//     pretendToBeVisual: true,
+//     referrer: 'https://formswift.com/',
+//     url: 'https://formswift.com/',
+//     userAgent: 'node.js',
+//   };
+//   const defaultJSDOM = new JSDOM(
+//     '<!DOCTYPE html><html><head></head><body></body></html>',
+//     defaultJSDOMSettings
+//   );
+//   global.window = defaultJSDOM.window;
+//   global.document = defaultJSDOM.window.document;
+
+//   /* WORKS (also all variants without global work) */
+//   // delete global.window.hello
+//   // global.window.hello = undefined
+
+//   /* DOESN'T WORK */
+//   // Object.assign(...)
+//   // Object.defineProperty(...) (on global)
+//   // Iterating through the keys on global.window and deleting any that aren't in defaultJSDOM.window (error)
+// })
+
+const originalDocumentAddEventListener = document.addEventListener
+
+let documentEventListeners = {};
+
 beforeEach(() => {
-  // Settings match my jest.config.js JSDOM config
-  const defaultJSDOMSettings = {
-    contentType: 'text/html',
-    pretendToBeVisual: true,
-    referrer: 'https://formswift.com/',
-    url: 'https://formswift.com/',
-    userAgent: 'node.js',
-  };
-  const defaultJSDOM = new JSDOM(
-    '<!DOCTYPE html><html><head></head><body></body></html>',
-    defaultJSDOMSettings
-  );
-  global.window = defaultJSDOM.window;
-  global.document = defaultJSDOM.window.document;
-
-  /* WORKS (also all variants without global work) */
-  // delete global.window.hello
-  // global.window.hello = undefined
-
-  /* DOESN'T WORK */
-  // Object.assign(...)
-  // Object.defineProperty(...) (on global)
-  // Iterating through the keys on global.window and deleting any that aren't in defaultJSDOM.window (error)
+  documentEventListeners = {}
+  jest.spyOn(document, 'addEventListener').mockImplementation((event, cb) => {
+    if (!documentEventListeners[event]) {
+      documentEventListeners[event] = [];
+    }
+    documentEventListeners[event].push(cb);
+    originalDocumentAddEventListener(event, cb)
+  });
 })
 
-/* SOLUTION FROM https://github.com/facebook/jest/issues/1224 */
-// const sideEffects = {
-//   document: {
-//     addEventListener: {
-//       fn: document.addEventListener,
-//       refs: [],
-//     },
-//     keys: Object.keys(document),
-//   },
-//   window: {
-//     addEventListener: {
-//       fn: window.addEventListener,
-//       refs: [],
-//     },
-//     keys: Object.keys(window),
-//   },
-// };
-
-// /* */
-// // Lifecycle Hooks
-// // -----------------------------------------------------------------------------
-// beforeAll(async () => {
-//   // Spy addEventListener
-//   ['document', 'window'].forEach(obj => {
-//     const fn = sideEffects[obj].addEventListener.fn;
-//     const refs = sideEffects[obj].addEventListener.refs;
-
-//     function addEventListenerSpy(type, listener, options) {
-//       // Store listener reference so it can be removed during reset
-//       refs.push({ type, listener, options });
-//       // Call original window.addEventListener
-//       fn(type, listener, options);
-//     }
-
-//     // Add to default key array to prevent removal during reset
-//     sideEffects[obj].keys.push('addEventListener');
-
-//     // Replace addEventListener with mock
-//     global[obj].addEventListener = addEventListenerSpy;
-//   });
-// });
-
-// // Reset JSDOM. This attempts to remove side effects from tests, however it does
-// // not reset all changes made to globals like the window and document
-// // objects. Tests requiring a full JSDOM reset should be stored in separate
-// // files, which is only way to do a complete JSDOM reset with Jest.
-// beforeEach(async () => {
-//   const rootElm = document.documentElement;
-
-//   // Remove attributes on root element
-//   [...rootElm.attributes].forEach(attr => rootElm.removeAttribute(attr.name));
-
-//   // Remove elements (faster than setting innerHTML)
-//   while (rootElm.firstChild) {
-//     rootElm.removeChild(rootElm.firstChild);
-//   }
-
-//   // Remove global listeners and keys
-//   ['document', 'window'].forEach(obj => {
-//     const refs = sideEffects[obj].addEventListener.refs;
-
-//     // Listeners
-//     while (refs.length) {
-//       const { type, listener, options } = refs.pop();
-//       global[obj].removeEventListener(type, listener, options);
-//     }
-
-//     // Keys
-//     Object.keys(global[obj])
-//       .filter(key => !sideEffects[obj].keys.includes(key))
-//       .forEach(key => {
-//         delete global[obj][key];
-//       });
-//   });
-
-//   // Restore base elements
-//   rootElm.innerHTML = '<head></head><body></body>';
-// });
+afterEach(() => {
+  Object.keys(documentEventListeners).forEach(eventName => {
+    documentEventListeners[eventName].forEach(cb => {
+      document.removeEventListener(eventName, cb);
+    });
+  });
+  /*
+    I tried doing same as above for window, but somehow the document listener
+    still fires when you click the window in Test 2, but not when you click the
+    document. This appears to be more of a full reset.
+  */
+  window._sessionHistory._windowImpl._eventListeners = Object.create( null
+  );
+  jest.restoreAllMocks();
+});
